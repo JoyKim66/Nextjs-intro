70ca76e0389298f5f103e68b1b537cd54a86dc8f
JoyKim66 <masscom6051@naver.com>
2023-01-23 21:15:14 +0900

    2.3 Server Side Rendering

--- pages/index.js
+++ pages/index.js
@@ -4,21 +4,13 @@ import { useEffect, useState } from "react";
 
 const API_KEY = "67c083c536b689fc74b429a01c0f65a0";
 
-export default function Home() {
-  const [movies, setMovies] = useState([]);
-  useEffect(() => {
-    (async () => {
-      const { results } = await (await fetch(`/api/movies`)).json();
-      setMovies(results);
-    })();
-  }, []);
+export default function Home({results}) {
   return (
     <div className="container">
       <Seo title="Home" />
-      {!movies && <h4>Loading...</h4>}
-      {movies?.map((movie) => (
+      {results?.map((movie) => (
         <div className="movie" key={movie.id}>
-          <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
+          <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} />
           <h4>{movie.original_title}</h4>
         </div>
       ))}
@@ -45,4 +37,13 @@ export default function Home() {
       `}</style>
     </div>
   );
+}
+
+export async function getServerSideProps() {
+  const { results } = await (await fetch(`http://localhost:3000/api/movies`)).json();
+  return {
+    props: {
+        results,
+    },
+  };
 }
